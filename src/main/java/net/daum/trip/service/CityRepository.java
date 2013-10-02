e55543e2d8da9bde8f5508f00d35aaf7a39ab47b
Myeongseok Baek <msbaek@daumcorp.com>
2013-10-02 16:18:07 +0900

    getCity 대신 findAll 호출하도록 변경

--- src/main/java/net/daum/trip/service/CityRepository.java
+++ src/main/java/net/daum/trip/service/CityRepository.java
@@ -21,7 +21,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
 
-interface CityRepository extends Repository<City, Long> {
+public interface CityRepository extends Repository<City, Long> {
 
 	Page<City> findAll(Pageable pageable);
 
--- src/main/java/net/daum/trip/web/controller/HelloController.java
+++ src/main/java/net/daum/trip/web/controller/HelloController.java
@@ -1,9 +1,14 @@
 package net.daum.trip.web.controller;
 
 import net.daum.trip.ServiceProperties;
+import net.daum.trip.domain.City;
+import net.daum.trip.service.CityRepository;
 import net.daum.trip.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,9 +22,33 @@ public class HelloController {
     @Autowired
     private CityService cityService;
 
+    @Autowired
+    private CityRepository cityRepoistory;
+
     @RequestMapping("/")
     @ResponseBody
-    public String helloWorld() {
-        return this.cityService.getCity("Bath", "UK").getName();
+    public Page<City> helloWorld() {
+        Pageable pageable = new Pageable() {
+            @Override
+            public int getPageNumber() {
+                return 1;
+            }
+
+            @Override
+            public int getPageSize() {
+                return 10;
+            }
+
+            @Override
+            public int getOffset() {
+                return 0;
+            }
+
+            @Override
+            public Sort getSort() {
+                return new Sort(Sort.Direction.ASC, "name");
+            }
+        };
+        return cityRepoistory.findAll(pageable);
     }
 }
